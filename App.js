3760ecf3c5af9983de033a9c3317c3bcc498dd17
Masatoshi Kana <mk120851@gmail.com>
2020-06-17 13:28:14 +0900

    salarate 2 pages

--- App.js
+++ App.js
@@ -1,47 +1,15 @@
 import React from "react";
-import { StyleSheet, Button, Text, View } from "react-native";
-import SafeAreaView from "react-native-safe-area-view";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Hello World!</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate("Details")}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
+import HomeScreen from "./components/HomeScreen";
+import InvoiceEditScreen from "./components/InvoiceEditScreen";
+import SummaryScreen from "./components/SummaryScreen";
 
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    InvoiceEdit: InvoiceEditScreen,
+    Summary: SummaryScreen,
   },
   {
     initialRouteName: "Home",
